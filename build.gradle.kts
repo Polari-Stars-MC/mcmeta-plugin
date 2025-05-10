8c9c19173c12914028f15874a88c8a2fc0b00a7f
baka4n <qq474899581@gmail.com>
2025-05-10 16:40:33 +0800

    更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    `maven-publish`
     kotlin("jvm").version("1.9.0")
     id("io.freefair.lombok").version("8.13.1")
+    id("io.github.jeadyx.sonatype-uploader").version("2.8")
 }
 
 val projGroup: String by rootProject
@@ -34,8 +34,30 @@ gradlePlugin {
     }
 }
 
-publishing {
-    repositories {
-        mavenLocal()
+sonatypeUploader {
+    tokenName = System.getenv("SONATYPE_PUBLISH_TOKEN")
+    tokenPasswd = System.getenv("SONATYPE_PASSWORD")
+    pom = Action {
+        name.set("MCMeta Plugin")
+        description.set("Minecraft metadata generation")
+        url.set("https://github.com/Polari-Stars-MC/mcmeta-plugin")
+        licenses {
+            license {
+                name.set("LGPL-2.1")
+                url = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html"
+            }
+        }
+        developers {
+            developer {
+                id.set("baka4n")
+                name.set("baka4n")
+                email.set("474899581@qq.com")
+            }
+        }
+        scm {
+            connection = "scm:git:git://github.com/Polaris-Stars-MC/mcmeta-plugin.git"
+            developerConnection = "scm:git:ssh://github.com/Polaris-Stars-MC/mcmeta-plugin.git"
+            url = "https://github.com/Polaris-Stars-MC/mcmeta-plugin"
+        }
     }
 }
