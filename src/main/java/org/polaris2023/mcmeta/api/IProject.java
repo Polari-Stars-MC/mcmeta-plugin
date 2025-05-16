82da52ec4f4ebc1bb2c150483bd7f6938025d7a1
baka4n <qq474899581@gmail.com>
2025-05-16 16:42:54 +0800

    doc抹除

--- src/main/java/org/polaris2023/mcmeta/api/IProject.java
+++ src/main/java/org/polaris2023/mcmeta/api/IProject.java
@@ -3,8 +3,8 @@ package org.polaris2023.mcmeta.api;
 import org.gradle.api.Project;
 
 /**
- * @author : baka4n
- * {@code @Date : 2025/05/12 11:16:58}
+ * @author baka4n
+ * {@code @Date 2025/05/12 11:16:58}
  */
 public interface IProject {
     Project project();
